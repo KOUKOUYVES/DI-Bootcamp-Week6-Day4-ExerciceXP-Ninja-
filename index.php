97621ee37172ac853569485d877fd82ceced6b27
KOUKOUYVES <wogelisabeth@gmail.com>
2023-02-03 14:31:10 +0000

    first

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body><br><br>
+    <h1 style="text-align: center;">Exercice Ninja Factorial Of A Number Using Recursive</h1>
+    <div class="container">
+<br><br>
+        <?php
+
+        $arr1 = array(1, 2, 3, 4,5);
+        $arr2 = array(2, 3, 4, 5, 6,7,8);
+
+        $arr1Andarr2Merge = array_merge($arr1, $arr2);
+        var_dump($arr1Andarr2Merge);
+        ?>
+    </div>
+</body>
+</html>
